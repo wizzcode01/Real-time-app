468efde8674791123d2428a335ef8db46080b516
Wisdom Dickson <dicksonwisdom005@gmail.com>
2025-11-14 04:21:27 -0700

    adding socket.js file

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 import express from 'express'
 import cors from 'cors'
 import http, { createServer } from 'http'
-import { Socket } from 'socket.io'
+import { Socketio } from 'socket.io'
 
 const app = express()
 //create http server and pass the app instance to it 
-const server = http.createServer(app)
+const httpServer = http.createServer(app)
+
+//initialize socket and attach it to http server to enable listening
+const io = Socketio(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
 
 const PORT = process.env.PORT
 server.listen(PORT, () => {
     console.log('Server is running ')
 })
+
+
+
+
---
+++ utils/socket.js
@@ -0,0 +1,53 @@
+import { Socket, Socketio } from "socket.io";
+import RoomModel from "../model/rooms";
+
+//listens for a new client websocket connection
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`)
+
+    let userRoomId = null
+    //logic to join a room
+    socket.on('joinRoom', async (roomName) => {
+        if(userRoomId){
+            socket.leave(userRoomId)
+        }
+
+        try {
+            const room = await RoomModel.findOne({name: roomName})
+            if(!room){
+                console.log(`Room ${roomName} not found`)
+                return 
+            }
+            userRoomId = room._id.toString()
+            socket.join(userRoomId) // register a socket to a room
+            console.log(`User ${socket.id} has joined: ${roomName}!`)
+            socket.emit('status', `Welcome to ${roomName}`)
+        }catch(error){
+            console.log('Error while joining room', error)
+        }
+    })
+
+    //send message to a room logic
+    socket.on('sendMessage', async(msg) => {
+        //msg contain user, message, roomName
+        //save message to mongo db for persistence
+        const room = await RoomModel.findOne({name: msg.roomName})
+       
+        const newMessage = ({
+            message: msg.message,
+            room: room._id,
+            user:msg.user
+        })
+       await newMessage.save()
+
+       //data for client broadcast
+       const clientMessage = {
+           message: newMessage.message,
+           user:newMessage.user,
+           timestamp: newMessage.timestamp,
+           room:room.name
+       }
+       //broadcast the message
+       socket.emit(clientMessage)
+    })
+})
