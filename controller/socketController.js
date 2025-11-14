24817f0003efda2cdba900bdbd7ca044150047b6
Wisdom Dickson <dicksonwisdom005@gmail.com>
2025-11-14 10:30:01 -0700

    added controller

---
+++ controller/socketController.js
@@ -0,0 +1,21 @@
+import ChatModel from "../model/chat";
+import RoomModel from "../model/rooms";
+
+export const getSocket = async (req , res) => {
+     try {
+        const roomName = req.params.roomName
+        const room = await RoomModel.findOne(roomName)
+        if(!room) {
+            return res.status(404).json({error: 'Room not found'})
+        }
+
+        //find chat by room id, sort by newest
+        const message  = await ChatModel.find({room : room._id})
+        .sort({timestamp: -1})
+        res.json(message.reverse())
+
+        }catch(err){
+        console.log("error from socker controller", err)
+        res.status(500).json({'server error'})
+        }
+}
